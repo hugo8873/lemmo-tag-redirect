631113625be28f67e7c5e365ac2a8dfb61bc9453
Hugo Z. Xu <22760386+hugo8873@users.noreply.github.com>
2026-08-18 19:08:09 +0800

    Update tagRedirect.js

--- api/tagRedirect.js
+++ api/tagRedirect.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 
   // ========== 改成你的真实地址 ==========
   const ZAPIER_HOOK = "https://hooks.zapier.com/hooks/catch/21644115/4tmbq9n/";
-  const TARGET_REDIRECT_URL = "https://www.kickstarter.com/projects/lemmothree/300815528/edit/preview";
+  const TARGET_REDIRECT_URL = "https://www.kickstarter.com/projects/lemmothree/lemmo-three-an-intelligent-all-purpose-vehicle-for-life";
   // =====================================
 
   if (!customer_id) {
